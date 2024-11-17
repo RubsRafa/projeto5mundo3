a2e3089f8973381dd25a698d95b29dc0aeaf5e1d
RubsRafa <rubiarafaela2000@gmail.com>
2024-11-16 21:18:16 -0300

    refactor: remove comments

--- CadastroClient/src/cadastroclient/CadastroClient.java
+++ CadastroClient/src/cadastroclient/CadastroClient.java
@@ -12,21 +12,17 @@ public class CadastroClient {
              ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
              ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
 
-            // Enviar login e senha
-            out.writeObject("op1"); // exemplo de login
-            out.writeObject("op1"); // exemplo de senha
+            out.writeObject("op1");
+            out.writeObject("op1");
 
-            // Receber resposta de validação do servidor
             String response = (String) in.readObject();
             System.out.println(response);
             if (response.contains("inválidas")) {
                 return;
             }
 
-            // Enviar comando 'L' para solicitar a lista de produtos
             out.writeObject("L");
 
-            // Receber e exibir a lista de produtos
             List<Produto> produtos = (List<Produto>) in.readObject();
             produtos.forEach(produto -> System.out.println(produto.getNome()));
 
